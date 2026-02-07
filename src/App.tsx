8c89944102e7105dd62eaae4695d729c6ccafebc
Jaiprakash L <jayprakash_88@yahoo.co.in>
2026-02-07 16:52:02 +0000

    Use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { CosmicScale } from './pages/CosmicScale';
+import { HumanCheck } from './pages/HumanCheck';
+import { DigitalRoadtrip } from './pages/DigitalRoadtrip';
+import { DopamineClicker } from './pages/DopamineClicker';
+import { ElementFusion } from './pages/ElementFusion';
+import { WebMuseum } from './pages/WebMuseum';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/WonderNew">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cosmic-scale" element={<CosmicScale />} />
+        <Route path="/human-check" element={<HumanCheck />} />
+        <Route path="/digital-roadtrip" element={<DigitalRoadtrip />} />
+        <Route path="/dopamine-clicker" element={<DopamineClicker />} />
+        <Route path="/element-fusion" element={<ElementFusion />} />
+        <Route path="/web-museum" element={<WebMuseum />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
